4e751e527b4456c8935e265edd240c0147442602
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-08-29 13:25:51 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+num=int (input("Enter the number : "))
+Factorial = 1
+if ( num>0):
+    for i in range(1,num+1):
+        Factorial = Factorial*i
+    print('The factorial of',num,'is',Factorial)
+#I.Selmeya-192121137

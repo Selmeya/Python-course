761c992897c31e21338e7bb7d1d32c59bc81c2f1
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-08-29 14:26:53 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,8 @@
+A1=(input("Enter any word or letter: "))
+A2 = (input("Enter any word or letter: "))
+if(sorted(A1)==sorted(A2)):
+    print(A1, 'and ' , A2,'are anagram')
+else:
+    print(A1,'and ',A2,'are not an anagram')
+#I.Selmeya- 192121137
+          
---
+++ No. of words &char in astring.py
@@ -0,0 +1,5 @@
+stringgg=(input("Enter the sentence or letter: "))
+res=len(stringgg.split())
+print('The number of words in the string:',(res))
+print('The number of character inn the string: ',len(stringgg))
+#I.Selmeya - 192121137

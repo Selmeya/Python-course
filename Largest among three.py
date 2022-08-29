643baa15e8ed688a4691732426e9bd884440f054
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-08-29 13:16:24 +0530

    Add files via upload

---
+++ Largest among three.py
@@ -0,0 +1,9 @@
+a = int(input("A= "))
+b = int (input("B= "))
+c= int(input("C= "))
+if( a >=b) and (a >=c):
+    print(a, 'is yhe largest')
+elif(b>=a) and ( b>= c):
+    print(b,'is the largest')
+else:
+    print(c, 'is the largest')
---
+++ Quadratic.py
@@ -0,0 +1,11 @@
+a=int(input(" A= "))
+b=int(input("B= "))
+c=int(input("C= "))
+#Quadratic equation = -b+-(b**2-4*a*c)**0.5/2*a
+d=(b**2-4*a*c)**0.5
+e=2*a
+Qua1=(-b+d)/e
+Qua2=(-b-d)/e
+print('The solution for the equation are',Qua1,'and',Qua2)
+#I.Selmeya
+#192121137

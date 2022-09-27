7a13c9f9274bd58128051a3eeb3708a851859dbc
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-27 14:29:41 +0530

    Add files via upload

---
+++ Reverse a String.py
@@ -0,0 +1,5 @@
+s = str(input("Enter a string: "))
+e = " "
+for i in (s):
+    e = i+e
+print ("Mirror image: ",e)
--- Sum n Difference of Max n Min in a List.py
+++ Sum n Difference of Max n Min in a List.py
@@ -1,4 +1,5 @@
 Selmiya = []
+
 s= int(input("Enter the number of elements you want to print in the list: "))
 for i in range (1,s+1):
     sel = int(input("Enter the %d element : "%i))
@@ -6,22 +7,28 @@ for i in range (1,s+1):
 print(Selmiya)
 Selmiya.sort()
 print(Selmiya)
+
 e = int(input("M = "))
 if( e>=s+1):
          print("Invalid input")
-l = int(input("N = "))
 
+Sum = Selmiya[e-1] +Selmiya[l-1]
+Diff = Selmiya[l-1] -Selmiya[e-1]
 if(l >=s+1):
          print("Invalid input")
 selmiya = Selmiya.sort()
 if(e == 1):
     print("The ", e ,"Maximum Value: ",Selmiya[-(e)])
-
-         
+    print("The Minimum Value : ",Selmiya[l-1])
+    Sum = Selmiya[-(e)] +Selmiya[l-1]
+    Diff = Selmiya[-(e)] - Selmiya[l-1]
+    print("The sum of minimum and maximum number is ",Sum)
+    print("The difference between minimum and maximum number is ",Diff)
 else:
     print("The ", l ,"Maximum Value: ",Selmiya[e-l])
-print("The Minimum Value : ",Selmiya[l-1])
-Sum = Selmiya[e-1]+Selmiya[l-1]
-Diff = Selmiya[e-1]- Selmiya[l-1]
-print("The sum of minimum and maximum number is ",Sum)
-print("The difference between minimum and maximum number is ",Diff)
+    print("The",e," Minimum Value : ",Selmiya[l-1])
+    print("The sum of minimum and maximum number is ",Sum)
+    print("The difference between minimum and maximum number is ",Diff)
+
+
+

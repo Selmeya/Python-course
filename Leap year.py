e9c6b8578ba264dac36712a02adc23f0ee43ee41
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-28 11:49:32 +0530

    Add files via upload

--- Leap year.py
+++ Leap year.py
@@ -1,7 +1,10 @@
 Year=int(input("Enter the year: "))
 if (Year %400 == 0) and (Year % 100):
     print(Year,'is a leap year')
+elif(Year<0):
+    print("Invalid input")
 elif(Year%4==0)and (Year%100!=0):
     print(Year,'is a leap year')
+
 else:
     print(Year,'is not a leap year')

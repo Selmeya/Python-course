3c440b52169bbf16d0e6c8fc26532c6c6b61d364
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-29 10:17:11 +0530

    Add files via upload

---
+++ Sorting Words.py
@@ -0,0 +1,9 @@
+Selmiya =[]
+s= int(input("Enter the number of elements you want to print : "))
+for i in range(s):
+    e= str(input(" "))
+    Selmiya.append(e)
+Selmiya.sort()
+for sel in Selmiya:
+    print(sel)
+    

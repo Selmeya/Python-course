f3fca5e9adee81a597e1c4bd8d8448dd080bd5ab
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-30 11:06:35 +0530

    Add files via upload

---
+++ Voting Eligibility.py
@@ -0,0 +1,8 @@
+Age=(input("Enter your age: "))
+S= 18 - Age
+if (Age >= 18):
+    print('You are eligible to vote ')
+elif (Age >= 'Eighteen'):
+    print('You are eligible to vote')
+else:
+    print('You are allowed to vote after',S,'years')

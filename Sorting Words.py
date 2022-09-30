4dc729e5161791cf0f79d40c67e49c228b7f3b75
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-30 10:11:58 +0530

    Add files via upload

--- Sorting Words.py
+++ Sorting Words.py
@@ -1,9 +1,15 @@
 Selmiya =[]
 s= int(input("Enter the number of elements you want to print : "))
+l= str(input("Order (A/D): "))
 for i in range(s):
     e= str(input(" "))
     Selmiya.append(e)
 Selmiya.sort()
-for sel in Selmiya:
-    print(sel)
+if (l == 'A'):
+    for sel in Selmiya:
+        print(sel)
+else:
+    Selmiya.sort(reverse=True)
+    for sel in Selmiya:
+        print(sel)
     
